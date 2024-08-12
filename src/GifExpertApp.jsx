f36d4a1ced5e6cc62ded251e81a5d91c86eb85f8
eddpool97 <edwin.e.ayala1997@gmail.com>
2024-08-12 14:24:13 -0600

    Agregar hook de manejo de estado de categorias

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,9 +1,33 @@
+import { useState } from 'react';
 
 
 export const GifExpertApp = ()=>{
+    
+    const [categories, setCategories] = useState(['One Punch']);
+
+    const onAddCategory = ()=>{
+        setCategories(['Nueva Categoria', ...categories]);
+    }
+    
     return(
         <>
+            {/*Titulo*/}
+
             <h1>GifExpertApp</h1>
+            
+            {/*Input*/}
+
+            {/*Listado de Gif*/}
+            <button onClick={onAddCategory}>Agregar</button>
+            <ol>
+                {
+                    categories.map( category =>{
+                        return <li key={category} >{ category }</li>
+                    })
+                }
+            </ol>
+
+            {/*Gif Item*/}
         </>
     )
 }
